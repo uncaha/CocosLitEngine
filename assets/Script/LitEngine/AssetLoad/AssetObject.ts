24e0ed18fe9c04309f364423bdc5db112ec21f3a
cczxntken <cczxntken@163.com>
2019-04-27 01:41:14 +0800

    fix

--- assets/Script/LitEngine/AssetLoad/AssetObject.ts
+++ assets/Script/LitEngine/AssetLoad/AssetObject.ts
@@ -1,27 +1,25 @@
-export default class AssetObject{
-    private _assetKey:string;
-    private _assetUsedCount:number = 0;
-    constructor(assetKey:string){
+export default class AssetObject {
+    private _assetKey: string;
+    private _assetUsedCount: number = 0;
+    constructor(assetKey: string) {
         this._assetKey = assetKey;
     }
 
-    public get assetKey()
-    {
+    public get assetKey() {
         return this._assetKey;
     }
 
-    public get assetUsedCount()
-    {
+    public get assetUsedCount() {
         return this._assetUsedCount;
     }
 
-    public retain()
-    {
+    public retain() {
         this._assetUsedCount += 1;
     }
 
-    public Release()
-    {
+    public Release() {
         this._assetUsedCount -= 1;
+        if (this._assetUsedCount < 0)
+            this._assetUsedCount = 0;
     }
 }
