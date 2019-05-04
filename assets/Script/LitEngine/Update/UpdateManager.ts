fe3731582d5bb7dcce51d92cd8f7d31142beb1c3
cczxntken <cczxntken@163.com>
2019-05-04 22:52:22 +0800

    fix

--- assets/Script/LitEngine/Update/UpdateManager.ts
+++ assets/Script/LitEngine/Update/UpdateManager.ts
@@ -54,9 +54,9 @@ export default class UpdateManager extends cc.Component {
     private _updateListener: any;
 
     private _updateState: UpdateStateType = UpdateStateType.none;
-    private checkUpdateCallback: ((arr:{ state: UpdateStateType,code?:number, msg?: string})=>void);
+    private checkUpdateCallback: ((arr: { state: UpdateStateType, code?: number, msg?: string }) => void);
 
-    private updateCallback: ((arr:{ state: UpdateStateType,updateObj?: UpdateProgress,code?:number, msg?: string})=>void);
+    private updateCallback: ((arr: { state: UpdateStateType, updateObj?: UpdateProgress, code?: number, msg?: string }) => void);
     private _upProgress: UpdateProgress;
 
     private static _instance: UpdateManager = null;
@@ -74,22 +74,20 @@ export default class UpdateManager extends cc.Component {
         UpdateManager.instance.manifestUrl = mf;
     }
 
-    public static isNeedUpdate(cb: ((arr:{ state: UpdateStateType,code?:number, msg?: string})=>void)) {
+    public static isNeedUpdate(cb: ((arr: { state: UpdateStateType, code?: number, msg?: string }) => void)) {
         UpdateManager.instance.checkUpdate(cb);
     }
 
-    public static StartUpdate(ucb:((arr:{ state: UpdateStateType,updateObj?: UpdateProgress,code?:number, msg?: string})=>void)) {
+    public static StartUpdate(ucb: ((arr: { state: UpdateStateType, updateObj?: UpdateProgress, code?: number, msg?: string }) => void)) {
         UpdateManager.instance.startUpdate(ucb);
     }
 
-    public GetState()
-    {
+    public GetState() {
         return this._updateState;
     }
 
-    public static Destory()
-    {
-        if(UpdateManager._instance)
+    public static Destory() {
+        if (UpdateManager._instance)
             UpdateManager._instance.node.destroy();
     }
 
@@ -198,7 +196,7 @@ export default class UpdateManager extends cc.Component {
 
         this._am.setEventCallback(null);
         if (this.checkUpdateCallback)
-            this.checkUpdateCallback({state: this._updateState, msg:tmsg,code:tcode});
+            this.checkUpdateCallback({ state: this._updateState, msg: tmsg, code: tcode });
     }
 
 
@@ -215,7 +213,7 @@ export default class UpdateManager extends cc.Component {
                 break;
             case jsb.EventAssetsManager.UPDATE_PROGRESSION:
                 this._upProgress.SetProgress(event);
-                this.updateCallback({state:UpdateStateType.updateing,updateObj:this._upProgress,code:tcode});
+                this.updateCallback({ state: UpdateStateType.updateing, updateObj: this._upProgress, code: tcode });
                 break;
             case jsb.EventAssetsManager.ERROR_DOWNLOAD_MANIFEST:
             case jsb.EventAssetsManager.ERROR_PARSE_MANIFEST:
@@ -256,7 +254,7 @@ export default class UpdateManager extends cc.Component {
         if (failed) {
             this._am.setEventCallback(null);
             this._updateListener = null;
-            this.updateCallback({state:this._updateState,code:tcode,msg:infoStr});
+            this.updateCallback({ state: this._updateState, code: tcode, msg: infoStr });
         }
 
         if (needRestart) {
@@ -273,7 +271,7 @@ export default class UpdateManager extends cc.Component {
             cc.sys.localStorage.setItem('HotUpdateSearchPaths', JSON.stringify(searchPaths));
             //console.log(JSON.stringify(searchPaths));
             jsb.fileUtils.setSearchPaths(searchPaths);
-            this.updateCallback({state:this._updateState,code:tcode});
+            this.updateCallback({ state: this._updateState, code: tcode });
             //cc.audioEngine.stopAll();
             //cc.game.restart();
 
@@ -293,9 +291,9 @@ export default class UpdateManager extends cc.Component {
         }
     }
 
-    checkUpdate(cb: ((arr:{ state: UpdateStateType,code?:number, msg?: string})=>void)) {
+    checkUpdate(cb: ((arr: { state: UpdateStateType, code?: number, msg?: string }) => void)) {
         if (this._updateState >= UpdateStateType.checking) {
-            cb({state :this._updateState,msg: 'checked.',code:UpdateStateType.none});
+            cb({ state: this._updateState, msg: 'checked.', code: UpdateStateType.none });
             return;
         }
 
@@ -309,7 +307,7 @@ export default class UpdateManager extends cc.Component {
         }
 
         if (!this._am.getLocalManifest() || !this._am.getLocalManifest().isLoaded()) {
-            cb({state :UpdateStateType.checkFailed,msg: 'Failed to load local manifest ...',code:UpdateStateType.none});
+            cb({ state: UpdateStateType.checkFailed, msg: 'Failed to load local manifest ...', code: UpdateStateType.none });
             return;
         }
 
@@ -319,7 +317,7 @@ export default class UpdateManager extends cc.Component {
         this._updateState = UpdateStateType.checking;
     }
 
-    startUpdate(ucb:((arr:{ state: UpdateStateType,updateObj?: UpdateProgress,code?:number, msg?: string})=>void)) {
+    startUpdate(ucb: ((arr: { state: UpdateStateType, updateObj?: UpdateProgress, code?: number, msg?: string }) => void)) {
         if (this._updateState > UpdateStateType.newVersion || this._updateState < UpdateStateType.updateFailed) return;
 
         if (this._am) {
